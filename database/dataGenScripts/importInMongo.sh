4a5927f249e9216425d220bfef4b699ccbb1bf3f
Nikolai Perlt <nikolaiperlt@gmail.com>
2020-05-15 13:03:57 +0200

    stuff

--- database/dataGenScripts/importInMongo.sh
+++ database/dataGenScripts/importInMongo.sh
@@ -1,7 +1,7 @@
 node generateProductData.js
 node generateOrderData.js
 
-mongo < deleteCollections.js
+mongo < deleteCollections.txt
 
 mongoimport --jsonArray --db jePerltRandomWebshop --collection products --file productData.json
 mongoimport --jsonArray --db jePerltRandomWebshop --collection orders --file orderData.json
