2b7ad0404fe7b30ecc1bde7977df613774aae184
Nikolai Perlt <nikolaiperlt@gmail.com>
2020-05-17 16:27:35 +0200

    stuff

--- database/databaseFacades/neo4jFacade.js
+++ database/databaseFacades/neo4jFacade.js
@@ -7,37 +7,38 @@ const driver = neo4j.driver(
 
 async function runDijkstra(city1, city2) {
   return new Promise(async (resolve, reject) => {
-    const session = driver.session({
-      database: "citydenmark.db"
-    });
-    let res = await session.run(`
-    MATCH(from: City{ name: $city1 }), (to: City{ name: $city2})
-    CALL apoc.algo.dijkstra(from, to, 'Road', 'time') yield path as path, weight as weight
-    RETURN path, weight
-    `, {
-      city1, city2
-    });
-    res.records.forEach(record => {
-      let segments = record.get("path")["segments"];
-      let nodes = segments.map(e => {
-        let { name, population, region } = e["start"]["properties"];
-        return {
-          name, population, region
-        };
+    try {
+      const session = driver.session({
+        database: "citydenmark.db"
+      });
+      let res = await session.run(`
+      MATCH(from: City{ name: $city1 }), (to: City{ name: $city2})
+      CALL apoc.algo.dijkstra(from, to, 'Road', 'time') yield path as path, weight as weight
+      RETURN path, weight
+      `, {
+        city1, city2
+      });
+      res.records.forEach(record => {
+        let segments = record.get("path")["segments"];
+        let nodes = segments.map(e => {
+          let { name, population, region } = e["start"]["properties"];
+          return {
+            name, population, region
+          };
+        })
+        let { name, population, region } = segments[segments.length - 1]["end"]["properties"];
+        nodes.push({ name, population, region })
+        resolve(nodes);
       })
-      let { name, population, region } = segments[segments.length - 1]["end"]["properties"];
-      nodes.push({ name, population, region })
-      console.log(nodes);
-    })
-
-
+    } catch (e) {
+      reject(e);
+    }
   })
-  resolve("LOL");
 }
 
 
 async function main() {
-  await runDijkstra("Copenhagen", "Hillerød");
+  console.log(await runDijkstra("Copenhagen", "Aalborg"));
 }
 
 main();
